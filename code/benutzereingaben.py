f4833e2a2004f056607ab52d25a0be5f4c7484f9
Kai Schell <kai.schell@oscert.eu>
2024-05-27 13:59:49 +0200

    update benutzereingaben

--- code/benutzereingaben.py
+++ code/benutzereingaben.py
@@ -1,11 +1,17 @@
 name = input("Bitte Namen eingeben: ")
-alter = input("Bitte Alter eingeben: ")
+alter = input("Bitte Alter als Zahl eingeben: ")
 # Explizites Casting (Umwandlung in einen bestimmten Datentyp)
-alter = int(alter)
 
-geburtsjahr = 2024 - alter
-## Ausgabe mit mehreren Argumenten durch Komma getrennt:
-#print("Hallo", name, ", du bist", alter, "Jahre alt.")
+# Prüfung, ob alter als Zahl eingegeben wurde
+if alter.isdigit():
+    alter = int(alter)
 
-# Ausgabe mit + als Konkatenationsoperator (Stringkonkatenation)
-print("Hallo " + name + ", du bist " + str(alter) + " Jahre alt, d.h. du bist im Jahr " + str(geburtsjahr) + " geboren.")
+    geburtsjahr = 2024 - alter
+    ## Ausgabe mit mehreren Argumenten durch Komma getrennt:
+    #print("Hallo", name, ", du bist", alter, "Jahre alt.")
+
+    # Ausgabe mit + als Konkatenationsoperator (Stringkonkatenation)
+    print("Hallo " + name + ", du bist " + str(alter) + " Jahre alt, d.h. du bist im Jahr " + str(geburtsjahr) + " geboren.")
+else:
+    print(str(alter) + "ist keine Zahl. Abbruch.")
+
