cf8da5d2be96d3bee35a6f4784b297d0cc7be1b1
Kai Schell <kai.schell@oscert.eu>
2024-05-29 09:15:26 +0200

    add strings and lists

---
+++ code/lists.py
@@ -0,0 +1,33 @@
+# Lists in Python
+
+# Eine list in Python ist so etwas wie ein Array in anderen Programmiersprachen
+
+# Variable, kann einen einzigen Wert speichern
+zahl = 4
+print("zahl: " + str(zahl))
+ 
+# List, kann beliebig viele Werte speichern
+# Element 1  2  3   4
+# Index:  0  1  2   3
+zahlen = [1, 3, 7, 14]
+
+print("zahlen: " + str(zahlen))
+
+print()
+print("List aus Integern: ")
+print()
+print("Ausgabe 1. Element am Index 0: " + str(zahlen[0]))
+print("Ausgabe 2. Element am Index 1: " + str(zahlen[1]))
+print("Ausgabe 3. Element am Index 2: " + str(zahlen[2]))
+
+# print("Versuch: Ausgabe 4. Element am Index 4: " + str(zahlen[4]))
+
+
+strings = ["Hund", "Katze", "Maus"]
+print()
+print("List aus Strings: ")
+print()
+print("Ausgabe 1. Element am Index 0: " + str(strings[0]))
+print("Ausgabe 2. Element am Index 1: " + str(strings[1]))
+print("Ausgabe 3. Element am Index 2: " + str(strings[2]))
+
---
+++ code/strings.py
@@ -0,0 +1,18 @@
+# Strings in Python
+
+my_string = "Ich bin ein String"
+
+print(my_string)
+
+my_second_string = " und ich noch einer"
+print(my_second_string)
+
+# Strings verbinden
+big_string = my_string + my_second_string
+print(big_string)
+
+for number in range(10):
+    print(number)
+
+for character in my_string:
+    print(character)
