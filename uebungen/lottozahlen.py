ee65a846a2f1b32f58ec7f5306a4649981fac8a6
Kai Schell <kai.schell@oscert.eu>
2024-05-31 14:12:24 +0200

    working version with counter

--- uebungen/lottozahlen.py
+++ uebungen/lottozahlen.py
@@ -8,9 +8,9 @@
 # - [x] -> zum Testen evtl. Liste mit Zahlen erstellen (schneller fürs Testen)
 # - [ ] Benutzereingaben prüfen
 # - [x] Tipp in einer Liste speichern
-# - [ ] Listen miteinander vergleichen
-# - [ ] -> nacheinander alle Werte aus Tipp mit Lottozahlen vergleichen
-# - [ ] Ausgabe wieviel Richtige
+# - [x] Listen miteinander vergleichen
+# - [x] -> nacheinander alle Werte aus Tipp mit Lottozahlen vergleichen
+# - [_] Ausgabe wieviel Richtige
 
 ## Schritt 2
 # - [ ] Programm solange Ziehungen machen, bis ihr 6 Richtige habt
@@ -18,64 +18,39 @@
 
 import random
 
-# num = random.randint(1, 49)    # 1 und 49 sind inklusive
-
-lottozahlen = []
-
-# lottozahlen.extend(range(1, 50))
-# print(lottozahlen)
-# lottozahlen = random.sample(lottozahlen, 6)
-# print(lottozahlen)
-
-while len(lottozahlen) < 6:
-    num = random.randint(1, 49) 
-    if num not in lottozahlen:
-        lottozahlen.append(num)
-print(lottozahlen)
-
-# TODO: User input bauen, fixe Liste nur zum Testen
-tipp = [3, 5, 32, 17, 9, 22]
-
 # Anahl der Richtigen
-correct = 0
-# Prüfe für jede Zahl in lottozahlen, ob diese in der Liste
-# tipp enthalten ist
-# falls ja, zähle die Anzahl der Richtigen um 1 hoch
-for number in lottozahlen:
-    if number in tipp:
-        correct += 1
+correct_numbers = 0
+attempts = 0
 
-print(f"Du hast {correct}: {lottozahlen} (richtige {tipp})")
+while correct_numbers < 6:
+    lottozahlen = []
+    correct_numbers = 0
 
+    attempts += 1
 
+    # lottozahlen.extend(range(1, 50))
+    # print(lottozahlen)
+    # lottozahlen = random.sample(lottozahlen, 6)
+    # print(lottozahlen)
 
+    while len(lottozahlen) < 6:
+        num = random.randint(1, 49) 
+        if num not in lottozahlen:
+            lottozahlen.append(num)
+    # print(lottozahlen)
 
+    # TODO: User input bauen, fixe Liste nur zum Testen
+    tipp = [3, 5, 32, 17, 9, 22]
 
+    # Prüfe für jede Zahl in lottozahlen, ob diese in der Liste
+    # tipp enthalten ist
+    # falls ja, zähle die Anzahl der Richtigen um 1 hoch
+    for number in lottozahlen:
+        if number in tipp:
+            correct_numbers += 1
 
-
-
-
-
-
-# counter = 0
-# print(num)
-
-# nums = []
-
-# while True:
-#     # user_guess = int(input("Bitte eine Zahl zwischen 1 und 10 eingeben: "))
-
-#     counter += 1
-
-#     if user_guess > num:
-#         print("zu gross, nochmal versuchen")
-#     elif user_guess < num:
-#         print("zu klein, nochmal versuchen")
-#     else:
-#         print("Gewonnen")
-#         break
-
-# print(f"Ende, du hast {counter} Versuche gebraucht.")
+print(f"Glückwunsch! Du hast {correct_numbers} Richtige: {lottozahlen} (Dein Tipp: {tipp})")
+print(f"Und dafür hast du nur {attempts} Versuche gebraucht.")
 
 
 
